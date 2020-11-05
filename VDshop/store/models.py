69b72769164dcea093debf58e0f8c960bd4bf61a
Valentine <valentyn.kofanov@knu.ua>
2020-11-05 21:12:38 +0200

    added Product modelC

--- VDshop/store/models.py
+++ VDshop/store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+
+    """ Product implementation """
+
+    name = models.CharField("Name", max_length=150)
+    description = models.TextField("Description")
+    photo_path = models.CharField("Photo path", max_length=150)  # path to the image of the product
+    price = models.IntegerField("Price")
