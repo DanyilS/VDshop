4bfaffb87cbace19e21c7f15c8c31af6353ccd17
Danyil <dselivanov2000@gmail.com>
2020-11-06 14:16:19 +0200

    Added view for product_detail page

--- VDshop/store/views.py
+++ VDshop/store/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
-from django.http import Http404, HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
+
 
 def home(request):
     return render(request, "store/home.html")
 
+
 def product_detail(request, slug):
 
+    product = get_object_or_404(Product, slug=slug)
 
+    context = {
+        'product': product,
+    }
 
-
-    #TODO: write template "store/templates/store/product_detail.html"
-    return render(request, '', )
+    # TODO: write template "store/templates/store/product_detail.html"
+    return render(request, '', context=context)
