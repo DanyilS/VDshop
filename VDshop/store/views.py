b9ef52019d0b48be72ba7160e87cd8c7f62b0cd6
Danyil <dselivanov2000@gmail.com>
2020-11-06 14:15:27 +0200

    Product model created

--- VDshop/store/views.py
+++ VDshop/store/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponse
 
-# Create your views here.
-
+from .models import Product
 
 def home(request):
     return render(request, "store/home.html")
+
+def product_detail(request, slug):
+
+
+
+
+    #TODO: write template "store/templates/store/product_detail.html"
+    return render(request, '', )
